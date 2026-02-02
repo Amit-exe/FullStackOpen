7b89ab6fb1b9eac373292cbaef4e13c0517e06ad
Amit-exe <amit.kushwaha.exe@gmail.com>
2026-02-02 23:45:31 +0530

    added  frontend

--- PlayGround/Backend/index.js
+++ PlayGround/Backend/index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json())
+
+app.use(express.static('dist'))
 const cusm = morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
